b3de8b010facf76d936399d4a8f65c2366cb7f13
Igor Garmatenko <igoren92@gmail.com>
2023-07-06 01:49:25 +0300

    terraform create ec2-srv

--- terraform/my-aws-infrastructure/ec2.tf
+++ terraform/my-aws-infrastructure/ec2.tf
@@ -1,7 +1,14 @@
-resource "aws_instance" "jenkins-master" {
+resource "aws_instance" "gitlab-srv" {
     instance_type = "t2.micro"
-
+    ami = "ami-0c65adc9a5c1b5d7c"
+    vpc_security_group_ids = ["sg-0c470427bf1e2955b"]
+    key_name = aws_key_pair.my_key.key_name
     tags = {
-        "Name" = "Jenkins-master"
+        "Name" = "Gitlab-server"
     }
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my_key"
+  public_key = file("/home/igor/.ssh/id_ed25519.pub")
 }
