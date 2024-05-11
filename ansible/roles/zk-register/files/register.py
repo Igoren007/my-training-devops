abfdb162a6586655be4389c35c7ab0dce6698d58
Igor Garmatenko <igoren92@gmail.com>
2024-05-11 07:21:24 +0300

    update zk-register

--- ansible/roles/zk-register/files/register.py
+++ ansible/roles/zk-register/files/register.py
@@ -1,17 +1,15 @@
-#!/usr/bin/env python3
 from kazoo.client import KazooClient
 import socket
 import time
 import signal
 from requests import get
+import subprocess
 
 
 SIGNAL_STATUS = True
 hostname = socket.gethostname()
-ip = socket.gethostbyname(hostname)
-#ip = get('https://api.ipify.org').content.decode('utf8')
-host_info = f"name:{hostname},ip:{ip}"
-
+addresses = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout
+host_info = f"name:{hostname},ip:{addresses}"
 
 def handler_stop_signal():
     if zk.exists(f"/nodes/{hostname}") is not None:
@@ -29,11 +27,11 @@ zk.start()
 
 if zk.exists("/nodes"):
     zk.create(f"/nodes/{hostname}", bytes(host_info, 'utf-8'))
-    print("done")
+    print(f"created node {hostname}")
 else:
     zk.create("/nodes")
     zk.create(f"/nodes/{hostname}", bytes(host_info, 'utf-8'))
-    print("done")
+    print(f"created node {hostname}")
 
 
 signal.signal(signal.SIGTERM, signal_handler)
